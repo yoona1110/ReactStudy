202ccbdd583330c952d1585a56b634907e3fb4a0
yoona1110 <gjdbsdk18@icloud.com>
2023-04-24 02:57:05 +0900

    2-2장 완료

--- ch02/ch02_2/src/index.tsx
+++ ch02/ch02_2/src/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 1. 배열과 JSX 구문
+const children1 = [
+  <li>
+    <a href="http://www.google.com" target="_blank">
+      <p> go to Google </p>
+    </a>
+  </li>,
+  <li>
+    <a href="http://www.naver.com" target="_blank">
+      <p> go to Naver </p>
+    </a>
+  </li>,
+  <li>
+    <a href="http://fackbook.com" target="_blank">
+      <p> go to Facebook </p>
+    </a>
+  </li>,
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 2. 데이터 배열을 컴포넌트 배열로 만들 때
+const children2 = [0, 1, 2].map((n: number) => <h3> Hello World! {n} </h3>)
+
+// XML 요소는 무조건 부모 요소가 있어야 함 / ex. <ul> </ul>
+const rootVirtualDOM = (
+  <ul>
+    <p> 1번 예제 </p>
+    {children1}
+    <br />
+    <p> 2번 예제 </p>
+    {children2}
+  </ul>
+)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(rootVirtualDOM)
