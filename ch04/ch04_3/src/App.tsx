02b87974f4d26580d8a17942080d4c0ba5d45e7c
yoona1110 <gjdbsdk18@icloud.com>
2023-06-14 00:35:53 +0900

    04-3장

--- ch04/ch04_3/src/App.tsx
+++ ch04/ch04_3/src/App.tsx
@@ -1,12 +1,14 @@
 import NumberState from './pages/NumberState'
 import InputTest from './pages/InputTest'
+import RadioInputTest from './pages/RadioInputTest'
 
 
 export default function App() {
   return (
     <div>
-      <NumberState/>
-      <InputTest/>
+      {/* <NumberState/>
+      <InputTest/> */}
+      <RadioInputTest/>
     </div>
   )
 }
